094e5d1f21eb56550220613fcbb3d99a95b2e763
grace-pasion <grpasion@outlook.com>
2025-03-10 22:12:01 -0600

    "in the process of greatest - muhammad ali"

--- server/src/main/java/dataaccess/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/MySQLGameDAO.java
@@ -1,13 +1,36 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
+import server.errors.ClassError;
+import server.errors.ServerExceptions;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 public class MySQLGameDAO implements GameDAO {
+
+    public MySQLGameDAO() throws ServerExceptions {
+        try {
+            DatabaseManager.createDatabase();  // Ensure database exists
+            configureDatabase();
+        } catch (DataAccessException e) {
+            throw new ServerExceptions(ClassError.DATABASE_ERROR);
+        }
+    }
+
     @Override
     public void clear() {
-
+        var statement = "TRUNCATE gameData";
+        try {
+            executeUpdate(statement);
+        } catch (DataAccessException | SQLException | ServerExceptions e) {
+            throw new RuntimeException("Error occurred when trying to clear");
+        }
     }
 
     @Override
@@ -22,7 +45,19 @@ public class MySQLGameDAO implements GameDAO {
 
     @Override
     public int generateGameID() {
-        return 0;
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT MAX(gameID) FROM gameData";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return rs.getInt(1)+1;
+                    }
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException("Error occurred when trying to create authentication");
+        }
+        return 1234;
     }
 
     @Override
@@ -33,5 +68,53 @@ public class MySQLGameDAO implements GameDAO {
     @Override
     public void updateGame(int gameID, GameData gameData) {
 
+    }
+
+    private void configureDatabase() throws ServerExceptions {
+        try (var conn = DatabaseManager.getConnection()) {
+            for (String statement: createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException | DataAccessException ex) {
+            throw new ServerExceptions(ClassError.DATABASE_ERROR);
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS gameData (
+                gameID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                whiteUsername VARCHAR(256) NOT NULL,
+                blackUsername VARCHAR(256) NOT NULL,
+                gameName VARCHAR(256) NOT NULL,
+                json TEXT DEFAULT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """
+    };
+
+    //int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game
+    private void executeUpdate(String statement, Object... params) throws DataAccessException, SQLException, ServerExceptions {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i =0;  i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i+1, p);
+                    else if (param instanceof Integer p) ps.setInt(i+1, p);
+                    else if (param instanceof ChessGame p) ps.setString(i+1, new Gson().toJson(p));
+                    else if (param == null) ps.setNull(i+1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    rs.getInt(1);
+                }
+
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Database update failed: " + e.getMessage());
+        }
     }
 }
