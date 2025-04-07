26b040f34a77ef59384fcc1fb563e4894b7c1d3c
grace-pasion <grpasion@outlook.com>
2025-04-06 19:57:08 -0600

    "websocket facade should be good"

--- client/src/main/java/websocketFacade/WebSocketFacade.java
+++ client/src/main/java/websocketFacade/WebSocketFacade.java
@@ -1,7 +1,12 @@
 package websocketFacade;
 
+import chess.ChessMove;
 import com.google.gson.Gson;
 import facade.exception.ResponseException;
+import websocket.commands.ConnectCommand;
+import websocket.commands.LeaveGameCommand;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.ResignCommand;
 import websocket.messages.ErrorMessage;
 import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
@@ -55,6 +60,44 @@ public class WebSocketFacade extends Endpoint { //extend endPoint
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void connect(String authToken, int gameID, ConnectCommand.Side side) {
+        try {
+            ConnectCommand command = new ConnectCommand(authToken, gameID, side);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void leave(String authToken, int gameID) {
+        try {
+            LeaveGameCommand command = new LeaveGameCommand(authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+            this.session.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void resign(String authToken, int gameID) {
+        try {
+            ResignCommand command = new ResignCommand(authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void makeMove(String authToken, int gameID, ChessMove move) {
+        try {
+            MakeMoveCommand command = new MakeMoveCommand(authToken, gameID, move);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
