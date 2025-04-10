8cf51756699e780ace06239908d31ed8ddfa388f
grace-pasion <grpasion@outlook.com>
2025-04-09 20:26:38 -0600

    "code quality check (part 2)"

--- client/src/main/java/ui/ChessBoardRender.java
+++ client/src/main/java/ui/ChessBoardRender.java
@@ -124,70 +124,6 @@ public class ChessBoardRender {
         out.println();
     }
 
-    /**
-     * Sets the correct initial pieces on the board for a
-     * white player
-     */
-    private void initializeWhite() {
-        chessBoard[7][0] = SET_TEXT_COLOR_WHITE + WHITE_ROOK;
-        chessBoard[7][1] = SET_TEXT_COLOR_WHITE + WHITE_KNIGHT;
-        chessBoard[7][2] = SET_TEXT_COLOR_WHITE + WHITE_BISHOP;
-        chessBoard[7][3] = SET_TEXT_COLOR_WHITE + WHITE_QUEEN;
-        chessBoard[7][4] = SET_TEXT_COLOR_WHITE + WHITE_KING;
-        chessBoard[7][5] = SET_TEXT_COLOR_WHITE + WHITE_BISHOP;
-        chessBoard[7][6] = SET_TEXT_COLOR_WHITE + WHITE_KNIGHT;
-        chessBoard[7][7] = SET_TEXT_COLOR_WHITE + WHITE_ROOK;
-
-        chessBoard[0][0] = SET_TEXT_COLOR_BLACK + BLACK_ROOK;
-        chessBoard[0][1] = SET_TEXT_COLOR_BLACK + BLACK_KNIGHT;
-        chessBoard[0][2] = SET_TEXT_COLOR_BLACK + BLACK_BISHOP;
-        chessBoard[0][3] = SET_TEXT_COLOR_BLACK + BLACK_QUEEN;
-        chessBoard[0][4] = SET_TEXT_COLOR_BLACK + BLACK_KING;
-        chessBoard[0][5] = SET_TEXT_COLOR_BLACK + BLACK_BISHOP;
-        chessBoard[0][6] = SET_TEXT_COLOR_BLACK + BLACK_KNIGHT;
-        chessBoard[0][7] = SET_TEXT_COLOR_BLACK + BLACK_ROOK;
-
-        for (int i = 0; i < 8; i++) {
-            chessBoard[6][i] = SET_TEXT_COLOR_WHITE + WHITE_PAWN;
-        }
-
-        for (int i = 0; i < 8; i++) {
-            chessBoard[1][i] = SET_TEXT_COLOR_BLACK + BLACK_PAWN;
-        }
-    }
-
-    /**
-     * Sets the correct initial pieces on the board for a
-     * black player
-     */
-    private void initializeBlack() {
-        chessBoard[7][0] = SET_TEXT_COLOR_BLACK + BLACK_ROOK;
-        chessBoard[7][1] = SET_TEXT_COLOR_BLACK + BLACK_KNIGHT;
-        chessBoard[7][2] = SET_TEXT_COLOR_BLACK + BLACK_BISHOP;
-        chessBoard[7][3] = SET_TEXT_COLOR_BLACK + BLACK_QUEEN;
-        chessBoard[7][4] = SET_TEXT_COLOR_BLACK + BLACK_KING;
-        chessBoard[7][5] = SET_TEXT_COLOR_BLACK + BLACK_BISHOP;
-        chessBoard[7][6] = SET_TEXT_COLOR_BLACK + BLACK_KNIGHT;
-        chessBoard[7][7] = SET_TEXT_COLOR_BLACK + BLACK_ROOK;
-
-        chessBoard[0][0] = SET_TEXT_COLOR_WHITE + WHITE_ROOK;
-        chessBoard[0][1] = SET_TEXT_COLOR_WHITE + WHITE_KNIGHT;
-        chessBoard[0][2] = SET_TEXT_COLOR_WHITE + WHITE_BISHOP;
-        chessBoard[0][3] = SET_TEXT_COLOR_WHITE + WHITE_QUEEN;
-        chessBoard[0][4] = SET_TEXT_COLOR_WHITE + WHITE_KING;
-        chessBoard[0][5] = SET_TEXT_COLOR_WHITE + WHITE_BISHOP;
-        chessBoard[0][6] = SET_TEXT_COLOR_WHITE + WHITE_KNIGHT;
-        chessBoard[0][7] = SET_TEXT_COLOR_WHITE + WHITE_ROOK;
-
-        for (int i = 0; i < 8; i++) {
-            chessBoard[6][i] = SET_TEXT_COLOR_BLACK + BLACK_PAWN;
-        }
-
-        for (int i = 0; i < 8; i++) {
-            chessBoard[1][i] = SET_TEXT_COLOR_WHITE + WHITE_PAWN;
-        }
-    }
-
     public void drawLegalMoves(PrintStream out, int row, boolean isWhite,
                                ChessBoard board, ChessPosition position,
                                ChessPiece piece) {
@@ -267,32 +203,35 @@ public class ChessBoardRender {
                     coloring(isWhite, isBlackSquare, row, col);
 
                 } else {
-                    if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
-                        chessBoard[row][col] = SET_TEXT_COLOR_BLACK;
-                        String symbol = switch (piece.getPieceType()) {
-                            case BISHOP -> BLACK_BISHOP;
-                            case ROOK -> BLACK_ROOK;
-                            case KNIGHT -> BLACK_KNIGHT;
-                            case QUEEN -> BLACK_QUEEN;
-                            case KING -> BLACK_KING;
-                            case PAWN -> BLACK_PAWN;
-                        };
-                        chessBoard[row][col] = chessBoard[row][col] +symbol;
-                    } else {
-                        chessBoard[row][col] = SET_TEXT_COLOR_WHITE;
-                        String symbol = switch (piece.getPieceType()) {
-                            case BISHOP -> WHITE_BISHOP;
-                            case ROOK -> WHITE_ROOK;
-                            case KNIGHT -> WHITE_KNIGHT;
-                            case QUEEN -> WHITE_QUEEN;
-                            case KING -> WHITE_KING;
-                            case PAWN -> WHITE_PAWN;
-                        };
-                        chessBoard[row][col] = chessBoard[row][col] +symbol;
-                    }
-
+                    assignPieces(piece, row, col);
                 }
             }
+        }
+    }
+
+    private void assignPieces(ChessPiece piece, int row, int col) {
+        if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
+            chessBoard[row][col] = SET_TEXT_COLOR_BLACK;
+            String symbol = switch (piece.getPieceType()) {
+                case BISHOP -> BLACK_BISHOP;
+                case ROOK -> BLACK_ROOK;
+                case KNIGHT -> BLACK_KNIGHT;
+                case QUEEN -> BLACK_QUEEN;
+                case KING -> BLACK_KING;
+                case PAWN -> BLACK_PAWN;
+            };
+            chessBoard[row][col] = chessBoard[row][col] +symbol;
+        } else {
+            chessBoard[row][col] = SET_TEXT_COLOR_WHITE;
+            String symbol = switch (piece.getPieceType()) {
+                case BISHOP -> WHITE_BISHOP;
+                case ROOK -> WHITE_ROOK;
+                case KNIGHT -> WHITE_KNIGHT;
+                case QUEEN -> WHITE_QUEEN;
+                case KING -> WHITE_KING;
+                case PAWN -> WHITE_PAWN;
+            };
+            chessBoard[row][col] = chessBoard[row][col] +symbol;
         }
     }
 
